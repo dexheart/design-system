b38cc542cd81665ad8ad0538cd15bb477c768d67
Tiago <tiago.linhares.dh@gmail.com>
2023-04-11 20:16:15 -0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# design-system
+# design-system Components: 
+
+- [ ] Text
+- [ ] Heading
+- [ ] Box
+- [ ] Button
+- [ ] TextInput
+- [ ] TextArea
+- [ ] CheckBox
+- [ ] Avatar
+- [ ] MultiStep
---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,7 @@
+export * from './colors'
+export * from './fonts'
+export * from './line-heights'
+export * from './radii'
+export * from './space'
+export * from './font-sizes'
+export * from './font-weights'
