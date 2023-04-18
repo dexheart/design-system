46678b392d9bdc0e08832b76eae86f8873afa610
Tiago <tiago.linhares.dh@gmail.com>
2023-04-18 19:45:21 -0300

    Fourth commit

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore architecto dignissimos dolorum commodi accusamus asperiores sit eveniet, alias, eos deserunt aspernatur quae numquam tempore voluptatibus voluptas. Earum commodi perspiciatis autem?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
